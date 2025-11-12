720269fd55c60f00c934df2b63ef9c98127528c3
Yehonatan Ezra <yonatan.ezra@mail.huji.ac.il>
2025-11-12 22:53:25 +0200

    hide keys

--- keys.py
+++ keys.py
@@ -1,12 +1,12 @@
 import os
 
 # Twilio credentials (use environment variables if available)
-ACCOUNT_SID = os.getenv("TWILIO_ACCOUNT_SID", "AC80205d6111f57aad69fd8c6bf831a81f")
-AUTH_TOKEN = os.getenv("TWILIO_AUTH_TOKEN", "a94ae0bdc739d44fc27e05fa7fadb98b")
+ACCOUNT_SID = os.getenv("TWILIO_ACCOUNT_SID", "")  # add your Account_Sid
+AUTH_TOKEN = os.getenv("TWILIO_AUTH_TOKEN", "")  # add you auth token
 
 # Phone numbers
-NUMBER_TWILIO = os.getenv("TWILIO_NUMBER", "+15187126855")
-YEHONATAN_NUMBER = os.getenv("YEHONATAN_NUMBER", "+972546374390")
+NUMBER_TWILIO = os.getenv("TWILIO_NUMBER", "") # add yout twilio number
+
 
 __all__ = [
     "ACCOUNT_SID",
@@ -19,3 +19,4 @@ __all__ = [
 def using_env_vars():
     """Return True if both main Twilio creds are set in the environment."""
     return bool(os.getenv("TWILIO_ACCOUNT_SID") and os.getenv("TWILIO_AUTH_TOKEN"))
+
